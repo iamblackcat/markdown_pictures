eb6a0ad4db5e20e07223b0bcec7193ce6c332700
iamblackcat <xjy_anonymous@163.com>
2019-06-14 12:14:26 +0800

    6/14

--- getpicture.py
+++ getpicture.py
@@ -1,19 +1,32 @@
 #-*-coding : utf-8-*-
 
 import requests
+import pyperclip
 import sys
-import time
 
-print(sys.argv[1])
+#print(sys.argv[1])
+current_time = time.time()
 
 def getPicture(url):
 	i = 0
 	imghtml = requests.get(url)
-	with open("%d.%s" %(time.time(),url[-3:]),'wb') as f:
+	with open("%d.%s" %(current_time,url[-3:]),'wb') as f:
 		f.write(imghtml.content)
 		f.close
 		i += 1
 
+"""def addToClipboard( string ):
+			from tkinter import Tk
+			r = Tk()
+			r.withdraw()
+			r.clipboard_clear()
+			r.clipboard_append(string)
+			r.update()
+			r.destroy()
+		"""		
+
 
 url = sys.argv[1]
 getPicture(url)
+address = "https://raw.githubusercontent.com/iamblackcat/markdown_pictures/master/%d.%s" % (current_time,url[-3:])	
+pyperclip.copy(address)
